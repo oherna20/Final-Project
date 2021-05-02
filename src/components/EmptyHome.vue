3145fe6f8de26704e2d06d6bcb6b6533c8e0eb47
oherna20 <orazalhernandez.oh@gmail.com>
2021-05-01 23:10:53 -0500

    Changed my theme to wrestling

--- src/components/EmptyHome.vue
+++ src/components/EmptyHome.vue
@@ -1,11 +1,11 @@
 <template>
-    <div
-      class="flex flex-col items-center justify-center min-h-screen space-x-4 text-6xl text-coolGray-300 font-extralight"
-    >
-      <div class="flex items-center justify-center space-x-4 animate-bounce">
-        <noto:film-frames />
-        <h1 class="">Hello There</h1>
-        <noto:film-projector />
-      </div>
+  <div
+    class="flex flex-col items-center justify-center min-h-screen space-x-4 text-6xl text-coolGray-300 font-extralight"
+  >
+    <div class="flex items-center justify-center space-x-4 animate-pulse">
+      <vaadin:group />
+      <h1 class="font-bold text-red-900">Welcome to the IMAX Experience</h1>
+      <fa-solid:theater-masks />
     </div>
-  </template>
+  </div>
+</template>
